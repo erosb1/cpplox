46e5c2c1117319ce94c01cff1ccf6dd324f05be6
Elias Rosberg <elias.rosberg@hotmail.com>
2024-06-13 14:35:47 +0200

    add unit tests for identifiers

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -37,7 +37,7 @@ BOOST_AUTO_TEST_CASE(LexerSingleCharTokens) {
     }
 }
 
-// Ensure that the Lexer correctly scans single character tokens
+// Check that the lexer correctly ignores whitespace and comments
 BOOST_AUTO_TEST_CASE(LexerIgnoreWhitespace) {
     std::string source_code = "  \n  +  \r\t -  //this is a comment\n / *";
     Lexer lexer(source_code);
@@ -55,7 +55,7 @@ BOOST_AUTO_TEST_CASE(LexerIgnoreWhitespace) {
     }
 }
 
-// Ensure that the Lexer correctly scans single character tokens
+// Check that the lexer can read numbers correctly
 BOOST_AUTO_TEST_CASE(LexerReadNumbers) {
     std::string source_code = "56.433 4 54 34 . 45. 54.132 234234";
     Lexer lexer(source_code);
@@ -77,8 +77,37 @@ BOOST_AUTO_TEST_CASE(LexerReadNumbers) {
     for (size_t i = 0; i < expectedTypes.size(); ++i) {
         BOOST_CHECK_EQUAL(tokens[i].type, expectedTypes[i]);
         BOOST_CHECK_EQUAL(tokens[i].lexeme, expectedLexemes[i]);
+        BOOST_CHECK_EQUAL(tokens[i].line, 0);
     }
 }
+
+
+// Check that the lexer can read numbers correctly
+BOOST_AUTO_TEST_CASE(LexerReadIdentifiers) {
+    std::string source_code = "far boo \n far23 23 bar. yolo_is_100";
+    Lexer lexer(source_code);
+
+    const std::vector<TT> expectedTypes = {
+        TT::IDENTIFIER, TT::IDENTIFIER, TT::IDENTIFIER, TT::NUMBER, TT::IDENTIFIER,
+        TT::DOT, TT::IDENTIFIER, TT::END,
+    };
+
+    const std::vector<std::string> expectedLexemes = {
+        "far", "boo", "far23", "23", "bar", ".", "yolo_is_100", "",
+    };
+
+    const std::vector<Token> tokens = lexer.Tokenize();
+
+    // Verify token properties
+    BOOST_REQUIRE_EQUAL(tokens.size(), expectedTypes.size());
+    for (size_t i = 0; i < expectedTypes.size(); ++i) {
+        BOOST_CHECK_EQUAL(tokens[i].type, expectedTypes[i]);
+        BOOST_CHECK_EQUAL(tokens[i].lexeme, expectedLexemes[i]);
+    }
+}
+
+
+
 
 // Check if the lexer can handle a basic variable declaration statement
 //BOOST_AUTO_TEST_CASE(LexerVarDeclaration) {
