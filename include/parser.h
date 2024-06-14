11783d139e0a214e8ed9cbfc2bfaf9b159f54db8
Elias Rosberg <elias.rosberg@hotmail.com>
2024-06-14 13:11:09 +0200

    setup basic error handling in Parser

--- include/parser.h
+++ include/parser.h
@@ -10,10 +10,17 @@ public:
     Program GenerateAST();
 private:
     void Advance();
+
+    // Error Handling
+    void ErrorAt(Token& token, std::string_view msg);
+    void ErrorAtCur(std::string_view msg);
+    void Synchronize();
 private:
     Lexer lexer_;
     Token prev_token_;
     Token cur_token_;
+    bool panic_mode_;  // switches between true/false when encountering errors and synchronizing
+    bool had_error_;   // remains true throughout entire parsing if had one error
 };
 
 
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include <iostream>
 
 Parser::Parser(std::string_view source_code)
     : lexer_(source_code)
@@ -23,4 +24,20 @@ void Parser::Advance() {
         if (cur_token_.type != TT::ERROR) break;
         // ErrorAtCur(token_.lexeme);
     }
+}
+
+void Parser::ErrorAt(Token &token, std::string_view msg) {
+    if (panic_mode_) return;
+    panic_mode_ = true;
+    had_error_ = true;
+    std::string error_type = token.type == TT::ERROR ? "[LEXING ERROR]" : "[PARSING ERROR]";
+    std::cerr << error_type << " at line: " << token.line << "\n\t" << msg << "\n";
+}
+
+void Parser::ErrorAtCur(std::string_view msg) {
+    ErrorAt(cur_token_, msg);
+}
+
+void Parser::Synchronize() {
+    //TODO implement
 }
