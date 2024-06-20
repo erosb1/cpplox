bcd5e7b3c94bcdd65bad126e783df20d1e3e4513
Elias Rosberg <elias.rosberg@hotmail.com>
2024-06-20 16:02:17 +0200

    add compiler class

--- include/compiler.h
+++ include/compiler.h
@@ -1,4 +1,32 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
+#include "parser.h"
+#include "semantic_analyser.h"
+#include "chunk.h"
+
+class Compiler : public ASTVisitor {
+public:
+    Chunk Compile(Program* program);
+    void visit(Program &node) override;
+    void visit(FunDecl &node) override;
+    void visit(VarDecl &node) override;
+    void visit(ExprStmt &node) override;
+    void visit(IfStmt &node) override;
+    void visit(PrintStmt &node) override;
+    void visit(ReturnStmt &node) override;
+    void visit(WhileStmt &node) override;
+    void visit(Block &node) override;
+    void visit(Assignment &node) override;
+    void visit(Binary &node) override;
+    void visit(Unary &node) override;
+    void visit(Call &node) override;
+    void visit(Identifier &node) override;
+    void visit(Literal &node) override;
+    void visit(Parameters &node) override;
+    void visit(Arguments &node) override;
+private:
+    Chunk cur_chunk_;
+};
+
 #endif //COMPILER_H
---
+++ src/compiler.cpp
@@ -0,0 +1,88 @@
+#include "compiler.h"
+
+Chunk Compiler::Compile(Program* program) {
+    const Chunk new_chunk_;
+    cur_chunk_ = new_chunk_;
+    program->accept(*this);
+    return cur_chunk_;
+}
+
+void Compiler::visit(Program &node) {
+    for (auto& declaration : node.declarations) {
+        declaration->accept(*this);
+    }
+}
+
+void Compiler::visit(FunDecl &node) {
+    // TODO implement functions
+    node.body->accept(*this);
+}
+
+void Compiler::visit(VarDecl &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(ExprStmt &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(IfStmt &node) {
+    node.condition->accept(*this);
+    node.if_body->accept(*this);
+    if (node.else_body != nullptr) node.else_body->accept(*this);
+}
+
+void Compiler::visit(PrintStmt &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(ReturnStmt &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(WhileStmt &node) {
+    node.condition->accept(*this);
+    node.body->accept(*this);
+}
+
+void Compiler::visit(Block &node) {
+    // PushScope
+    for (auto& declaration : node.declarations) {
+        declaration->accept(*this);
+    }
+    // PopScope
+}
+
+void Compiler::visit(Assignment &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(Binary &node) {
+    node.left_expression->accept(*this);
+    node.right_expression->accept(*this);
+}
+
+void Compiler::visit(Unary &node) {
+    node.expression->accept(*this);
+}
+
+void Compiler::visit(Call &node) {
+    node.arguments->accept(*this);
+}
+
+void Compiler::visit(Identifier &node) {
+
+}
+
+void Compiler::visit(Literal &node) {
+    cur_chunk_.AddConstant(node.value);
+}
+
+void Compiler::visit(Parameters &node) {
+}
+
+void Compiler::visit(Arguments &node) {
+    for (auto& expression : node.expressions) {
+        expression->accept(*this);
+    }
+}
